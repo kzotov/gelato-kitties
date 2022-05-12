4afb789f87267aa163e83a34f481b0e7d9c4f48f
Konstantin Zotov <konstantin.zotov@gmail.com>
2022-05-12 19:55:56 +0200

    Added heading to the NFT List

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,27 @@ import { Route, Routes } from 'react-router-dom';
 import Header from '@components/Header'
 import NftList from '@pages/NftList';
 import NftPage from '@pages/NftPage';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="app">
-      <Header />      
+      <Header />
       <div className="app__content">
+        <Typography
+          variant="h3"
+          noWrap
+          component="h3"
+          sx={{
+            mr: 2,
+            fontWeight: 700,
+            color: 'inherit',
+            textDecoration: 'none',
+            marginBottom: '30px',
+          }}
+        >
+          Explore Collections
+        </Typography>
         <Routes>
           <Route path="/" element={<NftList />}></Route>
           <Route path="/nfts/" element={<NftList />}></Route>
